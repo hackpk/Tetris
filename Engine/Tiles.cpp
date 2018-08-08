766af4072adc7e66469ed42c858c1f66636a6d72
Pooja Kadian <Kadian303@hotmail.com>
2018-08-08 18:10:06 +0530

    moveBy function is added to move the tiles

--- Engine/Tiles.cpp
+++ Engine/Tiles.cpp
@@ -10,3 +10,8 @@ void Tiles::Draw(Board & brd,const Color& in_color)
 {
 	brd.DrawCell(loc, in_color);
 }
+
+void Tiles::MoveBy(const Location & delta_loc)
+{
+	loc.Add(delta_loc);
+}
--- Engine/Tiles.h
+++ Engine/Tiles.h
@@ -6,8 +6,10 @@
 class Tiles
 {
 public:
+	Tiles() = default;
 	Tiles(const Location& in_loc);
 	void Draw(Board& brd,const Color& In_color);
+	void MoveBy(const Location& delta_loc);
 private:
 	Location loc;
 };
